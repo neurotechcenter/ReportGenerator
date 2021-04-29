0c8942f48f962380093e84bed2271a5e03600c17
James Swift <swift@neurotechcenter.org>
2021-04-29 13:16:31 -0500

    changed startup file name and added restoredefaultpath so you can run VERA and the report generator without closing matlab

---
+++ Startup_ReportGenerator.m
@@ -0,0 +1,14 @@
+% Startup function for ReportGenerator Version 5
+
+close all;
+clearvars;
+
+restoredefaultpath;
+
+addpath(genpath('./Major-programs'));
+addpath(genpath('./Nifti-toolbox'));
+addpath(genpath('./GUI Layout Toolbox'));
+addpath(genpath('./other-toolbox/export_fig'));
+addpath(genpath('./freesurfer-toolbox'));
+
+Rp = ReportPreviewer();
