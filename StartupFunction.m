2f410cc67b1e14dc8fcade0b29b812ffa64319ec
Yan Zhang <zhang@neurotechcenter.org>
2021-07-15 11:22:40 -0500

    deleted old startup function

--- StartupFunction.m
+++
@@ -1,8 +0,0 @@
-% Startup function for ReportGenerator Version 5
-addpath(genpath('./Major-programs'));
-addpath(genpath('./Nifti-toolbox'));
-addpath(genpath('./GUI Layout Toolbox'));
-addpath(genpath('./other-toolbox/export_fig'));
-addpath(genpath('./freesurfer-toolbox'));
-
-Rp = ReportPreviewer();
