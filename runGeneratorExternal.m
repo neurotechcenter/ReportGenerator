680bd8bcda99ab9fbe73ab7f75ef7205532f6fc9
James Swift <jamesswift@Jamess-MacBook-Pro-2.local>
2026-08-05 22:02:39 +0800

    fixed progress bar

--- runGeneratorExternal.m
+++ runGeneratorExternal.m
@@ -4,23 +4,27 @@ if ~isdeployed
     path=fileparts(mfilename('fullpath'));
     addpath(genpath(fullfile(path,'Major-programs')));
     addpath(genpath(fullfile(path,'Nifti-toolbox')));
-    addpath(genpath(fullfile(path,'GUI Layout Toolbox')));
+    addpath(genpath(fullfile(path,'GUI-Layout-Toolbox')));
     addpath(genpath(fullfile(path,'other-toolbox/export_fig')));
     addpath(genpath(fullfile(path,'freesurfer-toolbox')));
 end
 
     Rp = ReportPreviewer(projectPath);
+    bar = [];
     if(exist('UnifiedProgressBar','file') == 2) %if unified progressbar exists use it - for compatability with VERA
         bar=UnifiedProgressBar(Rp.gui,true); %supercede current bars
     end
     try
     uiwait(Rp.gui);
-    if(exist('UnifiedProgressBar','file') == 2) 
+    if ~isempty(bar)
         bar.Detach();
     end
     reportFilePath=Rp.OutPath;
-    catch
-        bar.Detach();
+    catch e
+        if ~isempty(bar)
+            bar.Detach();
+        end
+        rethrow(e);
     end
 end
 
